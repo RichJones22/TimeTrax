7443aaf0aca003e21b6b99c114dea712e94ce8b9
Rich Jones <jones_rich@yahoo.com>
2016-12-11 09:27:24 -0600

    misc changes

---
+++ packages/premise/utilities/src/PremiseUtilitiesProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Premise\Utilities;
+
+use Illuminate\Support\ServiceProvider;
+
+class PremiseUtilitiesProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->make(PremiseUtilities::class);
+    }
+}
