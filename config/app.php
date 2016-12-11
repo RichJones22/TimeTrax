19ae47a1c43155ebfc0a52574bde9786949858ee
Rich Jones <jones_rich@yahoo.com>
2016-12-11 11:46:11 -0600

    trying to add files to the submodule

--- config/app.php
+++ config/app.php
@@ -154,7 +154,7 @@ return [
         App\Providers\AppGlobalsServiceProvider::class,
         // TODO: removed; seem to be causing problems with 5.2 upgrade...
 //        Barryvdh\Debugbar\ServiceProvider::class,
-        Premise\Utilities\PremiseUtilitiesProvider::class
+//        Premise\Utilities\PremiseUtilitiesProvider::class
 
     ],
 
